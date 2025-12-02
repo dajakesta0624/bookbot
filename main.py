34d7dd1e0d906f804521c2cb97c6bd782922fc81
Jacob Campbell <dajakesta0624@gmail.com>
2025-12-01 23:28:19 -0600

    CH2L3 Read file

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+def get_book_text(filepath):
+    """Reads the contents of a file and returns it as a string."""
+    with open(filepath, 'r') as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    """Main function to print the contents of Frankenstein."""
+    book_text = get_book_text('frankenstein.txt')
+    print(book_text)
+
+if __name__ == '__main__':
+    main()
