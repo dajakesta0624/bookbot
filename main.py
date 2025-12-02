efeed60e968ac00932c2f0bb8e42482a03802da6
Jacob Campbell <dajakesta0624@gmail.com>
2025-12-01 23:58:25 -0600

    almost done 12/1

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 def get_book_text(filepath):
     """Reads the contents of a file and returns it as a string."""
-    with open(filepath, 'r', encoding='utf-8') as f:
+    with open(filepath, 'r') as f:
         file_contents = f.read()
     return file_contents
 
 def main():
     """Main function to print the contents of Frankenstein."""
     book_text = get_book_text('books/frankenstein.txt')
-    print(book_text)
+    print(book_text)  # This outputs the content, which can be redirected
 
 if __name__ == '__main__':
     main()
+
