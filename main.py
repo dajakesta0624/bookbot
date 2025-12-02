95d735bfe9d76a5d04880d1b25fa5c00f4422b54
Jacob Campbell <dajakesta0624@gmail.com>
2025-12-01 23:35:46 -0600

    fixing ch2l3

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 def get_book_text(filepath):
-    """Reads the contents of a file and returns it as a string."""
     with open(filepath, 'r') as f:
         file_contents = f.read()
     return file_contents
 
 def main():
-    """Main function to print the contents of Frankenstein."""
-    book_text = get_book_text('frankenstein.txt')
+    # Update the path to include the books folder
+    book_text = get_book_text('books/frankenstein.txt')
     print(book_text)
 
 if __name__ == '__main__':
